53efee21075cfdc388d791ade90b93cfc4cc8417
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-02-16 15:42:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import http from "node:http";
 
 import { serveStatic } from "./utils/serveStatic.js";
+import { getData } from "./utils/getData.js";
 
 const PORT = 8000;
 
@@ -8,14 +9,9 @@ console.log("CWD", process.cwd());
 
 const __dirname = import.meta.dirname;
 
-const server = http.createServer(async (req, res) => {
-  if (req.url === "/api") {
-    const data = { message: "Hello from the server!" };
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(data));
-    return;
-  }
+console.log(await getData());
 
+const server = http.createServer(async (req, res) => {
   await serveStatic(req, res, __dirname);
 });
 
