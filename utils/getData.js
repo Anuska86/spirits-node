35c2b565681e78c4e456dcc6b2121695f4a8503a
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-02-16 15:23:15 +0100

    Create getData.js

---
+++ utils/getData.js
@@ -0,0 +1,18 @@
+import path from "node:path";
+import fs from "node:fs/promises";
+
+export function getData() {
+  /*
+Challenge:
+1. getData() should: 
+    - read the json in json.data as a string 
+    - parse it to JS 
+    - return the parsed data. 
+
+   If there’s an error, it should return an empty array (think, why are we doing this?).
+
+hint.md for help
+*/
+
+  return "I am from getData()!";
+}
