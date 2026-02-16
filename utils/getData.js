ae7210b3396615403dbc2728ad9042b14581ebfa
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-02-16 15:32:52 +0100

    Update getData.js

--- utils/getData.js
+++ utils/getData.js
@@ -1,18 +1,15 @@
 import path from "node:path";
 import fs from "node:fs/promises";
 
-export function getData() {
-  /*
-Challenge:
-1. getData() should: 
-    - read the json in json.data as a string 
-    - parse it to JS 
-    - return the parsed data. 
+export async function getData() {
+  try {
+    const pathJSON = path.join("data", "data.json");
+    const data = await fs.readFile(pathJSON, "utf-8");
 
-   If there’s an error, it should return an empty array (think, why are we doing this?).
-
-hint.md for help
-*/
-
-  return "I am from getData()!";
+    const parsedData = JSON.parse(data);
+    return parsedData;
+  } catch (error) {
+    console.log("An error happened:", error);
+    return [];
+  }
 }
