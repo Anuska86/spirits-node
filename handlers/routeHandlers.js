3c862ae15dcd347f068a11cdc7c5b61789093ee7
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-02-16 17:44:47 +0100

    Update routeHandlers.js

--- handlers/routeHandlers.js
+++ handlers/routeHandlers.js
@@ -9,15 +9,4 @@ export async function handleGet(req, res) {
   sendResponse(res, 200, "application/json", content);
 }
 
-/*
-Challenge:
-1. Export a function called handleGet(). 
-2. It should:
-   - use getData() to get the data
-   - stringify that data
-   - use sendResponse() to serve it
-   
-Open the browser and load the sightings page to see if it works.
-*/
-
 // handlePost
